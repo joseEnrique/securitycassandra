895a5f05e70835aed7ef2ca2adc579183b72d21f
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-09-04 18:43:11 +0200

    possibility change systemauth to Network strategy

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import os
 from cassandra.cluster import Cluster, AuthenticationFailed
 from cassandra.auth import PlainTextAuthProvider
+from cassandra import ConsistencyLevel
 from time import sleep
 
 log = logging.getLogger()
@@ -10,9 +11,11 @@ log.setLevel('DEBUG')
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
 log.addHandler(handler)
+cluster = os.getenv('CASSANDRA_CLUSTER')
 newpass = os.getenv('CASSANDRA_PASSWORD')
 ip = os.getenv('CASSANDRA_IP')
 changed = False
+sleep(10)
 while changed is False:
     try:
         auth_provider = PlainTextAuthProvider(
@@ -20,6 +23,9 @@ while changed is False:
         cluster = Cluster(contact_points=[ip] , auth_provider=auth_provider)
         session = cluster.connect()
         rows = session.execute("ALTER ROLE cassandra WITH PASSWORD='"+newpass+"';")
+        if cluster is "yes":
+            query = SimpleStatement("ALTER KEYSPACE system_auth WITH replication = { 'class' : 'NetworkTopologyStrategy','dc1': 2};", consistency_level=ConsistencyLevel.ONE)
+            session.execute(query)
         changed = True
         log.warning("Changed")
     except Exception as ex:
